7dbbde74b835804448d162c8070dcc042af8bdd1
iell02 <rafael.cdeoliveira@yahoo.com.br>
2020-11-05 10:50:38 -0300

    webrook

--- pipeline/pipeline2.groovy
+++ pipeline/pipeline2.groovy
@@ -40,7 +40,7 @@ if (params.Continuar != 'Sim') {
                 currentBuild.result = 'FAILURE'
             }
 			archiveArtifacts '*.txt'
-			office365ConnectorSend message: 'Execucao Concluida.', status: 'End', webhookUrl: 'https://outlook.office.com/webhook/f1d4a645-1793-4be1-88e9-ccd82e5c37c8@93f33571-550f-43cf-b09f-cd331338d086/JenkinsCI/f4ab83d5fb7842b59f6b59fd8c0d1c0b/f1d4a645-1793-4be1-88e9-ccd82e5c37c8'
+			office365ConnectorSend message: 'Execucao Concluida.', status: 'End', webhookUrl: 'https://outlook.office.com/webhook/99c74417-560f-40d5-9527-973526673392@93f33571-550f-43cf-b09f-cd331338d086/JenkinsCI/0aaeed8767fe432b900ea9e97cd61437/99c74417-560f-40d5-9527-973526673392'
         }
 	}
 }
